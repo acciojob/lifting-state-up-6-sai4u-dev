4ce3fc7cde95c1566e0bb651c81a2c450f85a45c
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-21 08:50:44 +0000

    Update App.js + Added TodoList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';\
+import '../styles/App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', isCompleted: false },
+    { id: 2, text: 'Build a React app', isCompleted: false },
+    { id: 3, text: 'Deploy the React app', isCompleted: false },
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ padding: '20px' }}>
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function TodoList({ todos, handleComplete }) {
+    return (
+        <div>
+            <h2>Child Component</h2>
+            <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
+                {todos.map(todo => (
+                    <li key={todo.id} style={{ margin: '10px 0' }}>
+                        <span style={{
+                            textDecoration: todo.isCompleted ? 'line-through' : 'none',
+                            color: todo.isCompleted ? 'gray' : 'black'
+                        }}>
+                            {todo.text}
+                        </span>
+
+                        {!todo.isCompleted && (
+                            <button
+                                onClick={() => handleComplete(todo.id)}
+                                style={{ marginLeft: '10px' }}
+                            >
+                                Complete
+                            </button>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
