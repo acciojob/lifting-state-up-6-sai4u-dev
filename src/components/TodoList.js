f29817be24f27fc77f6b3314b60fecf5fa370f48
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-26 00:42:54 +0000

    p

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
 function TodoList({ todos, handleComplete }) {
-  // Find the first incomplete todo
-  const firstIncomplete = todos.find(todo => !todo.isCompleted);
-
   return (
     <div>
       <h2>Child Component</h2>
       <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
         {todos.map(todo => (
           <li key={todo.id} style={{ margin: '10px 0' }}>
+            {/* Display the todo text */}
             <span
+              data-cy={`todo-text-${todo.id}`}  // ensures Cypress can target this todo
               style={{
                 textDecoration: todo.isCompleted ? 'line-through' : 'none',
-                color: todo.isCompleted ? 'gray' : 'black',
+                color: todo.isCompleted ? 'gray' : 'black'
               }}
             >
               {todo.text}
             </span>
 
-            {/* Only render the button for the first incomplete todo */}
-            {firstIncomplete && todo.id === firstIncomplete.id && (
+            {/* Show "Complete" button only if todo is not completed */}
+            {!todo.isCompleted && (
               <button
+                data-cy={`complete-btn-${todo.id}`}  // ensures Cypress can click this button
                 onClick={() => handleComplete(todo.id)}
                 style={{ marginLeft: '10px' }}
               >
