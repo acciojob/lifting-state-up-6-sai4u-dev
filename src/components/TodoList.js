096df7efc7b16fcb4a574e17774c472251e34142
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-26 00:39:40 +0000

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,33 +1,38 @@
 import React from 'react';
 
 function TodoList({ todos, handleComplete }) {
-    return (
-        <div>
-            <h2>Child Component</h2>
-            <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
-                {todos.map(todo => (
-                    <li key={todo.id} style={{ margin: '10px 0' }}>
-                        <span style={{
-                            textDecoration: todo.isCompleted ? 'line-through' : 'none',
-                            color: todo.isCompleted ? 'gray' : 'black'
-                        }}>
-                            {todo.text}
-                        </span>
+  // Find the first incomplete todo
+  const firstIncomplete = todos.find(todo => !todo.isCompleted);
 
-                        {!todo.isCompleted && (
-                            <button
-                                data-cy={`complete-btn-${todo.id}`}
-                                onClick={() => handleComplete(todo.id)}
-                                style={{ marginLeft: '10px' }}
-                            >
-                                Complete
-                            </button>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
+        {todos.map(todo => (
+          <li key={todo.id} style={{ margin: '10px 0' }}>
+            <span
+              style={{
+                textDecoration: todo.isCompleted ? 'line-through' : 'none',
+                color: todo.isCompleted ? 'gray' : 'black',
+              }}
+            >
+              {todo.text}
+            </span>
+
+            {/* Only render the button for the first incomplete todo */}
+            {firstIncomplete && todo.id === firstIncomplete.id && (
+              <button
+                onClick={() => handleComplete(todo.id)}
+                style={{ marginLeft: '10px' }}
+              >
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoList;
